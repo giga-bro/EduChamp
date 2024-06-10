571d43bbfe7547ccc27134eeaa8b75d5c044e427
giga-bro <aditya.sakpal2008@gmail.com>
2024-06-11 00:12:25 +0530

    Update Quiz.tsx

--- frontend/src/components/Quiz/Quiz.tsx
+++ frontend/src/components/Quiz/Quiz.tsx
@@ -169,7 +169,7 @@ const Quiz = () => {
 
     try {
       console.log(score,testId);
-      const res = await axios.post('http://localhost:5000/api/results/storeResults',{ 
+      const res = await axios.post('https://educhamp.onrender.com/api/results/storeResults',{ 
         username: localStorage.getItem('username'),
         testId:testId,
         score: score,
