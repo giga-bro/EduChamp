6713f0036fed21d7f5c8a3ba64865f297767b7ee
giga-bro <aditya.sakpal2008@gmail.com>
2024-06-11 08:57:36 +0530

    Update SignIn.tsx

--- frontend/src/components/Auth/SignIn.tsx
+++ frontend/src/components/Auth/SignIn.tsx
@@ -111,7 +111,7 @@ export const GoogleButton = () => {
     });
     return (
         <a className="social-icon lg:!px-[20%] px-[15%] " id="googleIcon" onClick={() => signIn()}  >
-            <p className='font-bold text-xs  lg:text-sm flex text-center lg:text-center justify-end items-center  lg:w-[100%] lg:mr-[5%] lg:mb-[2%] lg:border-black w-[90%] ' >Login with Google</p><i className="fab fa-google  " />
+            <p className='font-bold text-xs  lg:text-sm flex text-center lg:text-center justify-end items-center  lg:w-[100%] lg:mr-[5%] lg:mb-[2%] lg:border-black w-[90%]  mr-[10%]  ' >Login with Google</p><i className="fab fa-google  " />
         </a>
     );
 }
